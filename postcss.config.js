126d255a0c662531e8a0d52163178b17b0051cee
Sergeev Sergey <sse1595@gmail.com>
2019-07-09 12:58:11 +0300

    cssnano for prod

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
+const plugins = [
+    require('autoprefixer'),
+    require('postcss-modules-values')
+];
+
+console.log('POSTCSS SEREGA!', process.env.NODE_ENV)
+if (process.env.NODE_ENV === 'production') {
+    plugins.push(require('cssnano')({ preset: 'default' }))
+}
+
 module.exports = {
-    plugins: [
-      require('autoprefixer'),
-      require('postcss-modules-values')
-    ]
+    plugins
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
 import 'normalize.css';
 import './theme/global.css';
+import App from './components/App';
             
 render(
     <App initialCount={0}/>,
