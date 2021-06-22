10d305fa8cb1890d4c413a31f86d934e18d27419
Sergeev Sergey <sse1595@gmail.com>
2021-06-22 18:25:23 +0300

    linter and prettier setup

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import style from './style.css';
 
-const a = 1;
-
-console.log('husky');
-
 const App = () => {
     return <h1 className={style.title}>Basic React Boilerplate</h1>;
 };
